484f3b5013de2589768af4c944b07ef8f95d054e
marinus-g <marinusmail@protonmail.com>
2024-06-21 11:32:13 +0200

    Initial Commit

---
+++ src/main/java/academy/mischok/modules/ModulesBackendApplication.java
@@ -0,0 +1,13 @@
+package academy.mischok.modules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModulesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModulesBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/academy/mischok/modules/ModulesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.mischok.modules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModulesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
