9738d66a67b48bd033d1fd2f744a1ea2b65b0272
marinus-g <marinusmail@protonmail.com>
2024-07-01 12:27:26 +0200

    MM-43 disabled csrf

--- src/main/java/academy/mischok/modules/configuration/WebSecurityConfiguration.java
+++ src/main/java/academy/mischok/modules/configuration/WebSecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -35,6 +36,7 @@ public class WebSecurityConfiguration {
                             source.registerCorsConfiguration("/**", configuration.applyPermitDefaultValues());
                             return configuration;
                         }))
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
                                 .requestMatchers("/error", "/oauth/token").permitAll()
