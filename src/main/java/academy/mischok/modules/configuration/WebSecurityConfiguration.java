378b7d6993139e4c5f26a842b81a3d2b211a3e3d
marinus-g <marinusmail@protonmail.com>
2024-07-01 10:02:15 +0200

    MM-43 added custom security config

---
+++ src/main/java/academy/mischok/modules/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,48 @@
+package academy.mischok.modules.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+
+@Configuration
+public class WebSecurityConfiguration {+
+
+    private final String redirectUrl;
+
+    public WebSecurityConfiguration(
+            @Value("${spring.cloud.azure.active-directory.redirect-uri-template}")
+            String redirectUrl) {
+        this.redirectUrl = redirectUrl;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return
+                http
+                        .cors(configurer -> {
+                            configurer.configurationSource(request -> {
+                                var cors = new org.springframework.web.cors.CorsConfiguration();
+                                cors.setAllowedOrigins(List.of("*"));
+                                cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+                                return cors;
+                            });
+                        })
+                        .oauth2Login(configurer ->  {
+                            configurer
+                                    .loginPage("/oauth2/authorization/microsoft")
+                                    .defaultSuccessUrl("/home", false);
+                        })
+                        .authorizeHttpRequests(configurer -> {
+                            configurer
+                                    .requestMatchers("/oauth2/authorization/microsoft", "/error").permitAll()
+                                    .anyRequest().authenticated();
+
+                        })
+                        .build();
+    }
+}
