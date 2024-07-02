7dd58ce1af86fadcc41a396544098f57adef899a
marinus-g <marinusmail@protonmail.com>
2024-07-02 08:37:06 +0200

    MM-43 testing

--- src/main/java/academy/mischok/modules/configuration/WebSecurityConfiguration.java
+++ src/main/java/academy/mischok/modules/configuration/WebSecurityConfiguration.java
@@ -30,6 +30,7 @@ public class WebSecurityConfiguration {
                         httpSecurityCorsConfigurer.configurationSource(request -> {
                             CorsConfiguration configuration = new CorsConfiguration();
                             configuration.setAllowedOriginPatterns(List.of("*"));
+                            configuration.setAllowedOrigins(List.of("http://localhost:8080"));
                             configuration.setAllowCredentials(true);
                             configuration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT", "OPTIONS"));
                             configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
