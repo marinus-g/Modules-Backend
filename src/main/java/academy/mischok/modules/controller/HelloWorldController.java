a06b013db86b6a0b77913296e1a1715dac1f4f5e
marinus-g <marinusmail@protonmail.com>
2024-07-04 14:19:25 +0200

    MM-43 testing

--- src/main/java/academy/mischok/modules/controller/HelloWorldController.java
+++ src/main/java/academy/mischok/modules/controller/HelloWorldController.java
@@ -18,7 +18,7 @@ public class HelloWorldController {
     @GetMapping
     public ResponseEntity<List<String>> test(@AuthenticationPrincipal OidcUser user) {
         System.out.println("XDXDXD");
-        user.getAuthorities().forEach(grantedAuthority -> System.out.println(grantedAuthority.getAuthority());
+        user.getAuthorities().forEach(grantedAuthority -> System.out.println(grantedAuthority.getAuthority()));
         return ResponseEntity.ok(List.of("Hello World", user.getFullName(), user.getAuthorities()
                 .stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining(", "))));
     }
