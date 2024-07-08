8bfed8d0ddd079fb8ebe51d1ab0ad47a2c470bc1
Bockkwurst <147827734+Bockkwurst@users.noreply.github.com>
2024-07-08 13:13:07 +0200

    deploy

--- src/main/java/academy/mischok/modules/controller/ModuleViewController.java
+++ src/main/java/academy/mischok/modules/controller/ModuleViewController.java
@@ -1,5 +1,6 @@
 package academy.mischok.modules.controller;
 
+import academy.mischok.modules.dtos.ModuleDto;
 import academy.mischok.modules.dtos.ModuleOverviewDto;
 import academy.mischok.modules.repository.ClassModuleRepository;
 import academy.mischok.modules.service.ModuleViewService;
@@ -9,6 +10,7 @@ import lombok.Setter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -26,6 +28,14 @@ public class ModuleViewController {
     @GetMapping("/modules/{moduleId}")
     public ResponseEntity<List<ModuleOverviewDto>> getModuleOverviewByModuleId(@PathVariable Long moduleId) {
         List<ModuleOverviewDto> modules = moduleViewService.getModuleOverviewByModuleId(moduleId);
+        return ResponseEntity.ok(modules);
+    }
+
+    @GetMapping("/modules/search/{name}")
+    public ResponseEntity<List<ModuleDto>> searchModulesByName(@RequestParam String name) {
+        List<ModuleDto> modules = moduleViewService.getModuleByName(name)
+                .stream()
+                .toList();
         return ResponseEntity.ok(modules);
     }
 }
